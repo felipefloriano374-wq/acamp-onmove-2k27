e784659723eee2d63292e81d9e4d6a2728de1fbf
Djhon20 <felipe.floriano374@gmail.com>
2026-07-28 09:01:03 -0300

    feat

--- acamp-onmove-2k27/src/main.jsx
+++ acamp-onmove-2k27/src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/home/index'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <Home/>
 )
